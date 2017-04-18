a595f8091a567fb9c3560b03c89c7ec0b45201de
jogurtfromcuba <oleg.filatov@phystech.edu>
2017-04-18 21:33:37 +0300

    took crab.py from the last commit on Bc branch and used virt factory in Bc fit

--- Bfinder.cc
+++ Bfinder.cc
@@ -841,9 +841,9 @@ for(vector<pat::GenericParticle>::const_iterator iKaonM = thePATTrackHandle->beg
                          if (!Bs_TT.isValid()) continue;
 
                          vector<RefCountedKinematicParticle> vFitMCParticlesBc;
-                         vFitMCParticlesBc.push_back(pFactory.particle(Bs_TT, Bs_mass_cjp_tmp, chi, ndf, PM_muon_sigma));
+                         //vFitMCParticlesBc.push_back(pFactory.particle(Bs_TT, Bs_mass_cjp_tmp, chi, ndf, PM_muon_sigma));
                          //vFitMCParticlesBc.push_back(BsCandCjp);
-                         //vFitMCParticlesBc.push_back(vFactory.particle(BsCandCjp->currentState(), Bs_fit_chi2, Bs_dof, BsCandCjp));
+                         vFitMCParticlesBc.push_back(vFactory.particle(BsCandCjp->currentState(), Bs_fit_chi2, Bs_dof, BsCandCjp));
                          //vFitMCParticlesBc.push_back(pFactory.particle(bCandMC->refittedTransientTrack(),bs_mass,chi,ndf,bs_sigma));
                          vFitMCParticlesBc.push_back(pFactory.particle(pionTT, PM_PDG_PION_MASS, chi,ndf, PM_pion_sigma));
 
