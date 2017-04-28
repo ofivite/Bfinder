8e9b7a0afceaddeb5bdb9eb3104dbffa2fc1c06c
jogurtfromcuba <oleg.filatov@phystech.edu>
2017-04-28 10:21:44 +0300

    modified MySel

--- MySelector_B0.py
+++ MySelector_B0.py
@@ -6,7 +6,7 @@ isMC = 0
 _fileOUT = 'Bc_v2_parked_notall.root'
 
 MyFileNamesMC = glob.glob( MCpath(1) + "*.root")
-MyFileNamesDA = glob.glob("/afs/cern.ch/work/o/ofilatov/CMSSW_5_3_24/src/XbFrame/Xb_frame/crab_projects_Bfinder_Bc_v2_parked/crab_Bfinder_Bc_v2_par*/results/*.root")
+MyFileNamesDA = (glob.glob("/afs/cern.ch/work/o/ofilatov/CMSSW_5_3_24/src/XbFrame/Xb_frame/crab_projects_Bfinder_B0_pho_v1/cr*/results/*.root"))
 
 ##__aa = 0;    __bb = 10
 __aa = 0;  __bb =  len(MyFileNamesDA);
@@ -111,7 +111,7 @@ for evt in range(0, nEvt):
     ##
     if (ch.GetEntry(evt) <= 0) : break;
     BInfo_size  = ch.nB
-    if len(ch.mum_dxy_Bsdecay) != BInfo_size:
+    if len(ch.B_mu_px1_cjp) != BInfo_size:
 ##        print 'Sizes do not match!', 'array len = ', len(ch.mum_dxy_Bsdecay), ' nB = ', BInfo_size
         match_i += 1
         continue
@@ -131,15 +131,15 @@ for evt in range(0, nEvt):
         MUMUP4_C0    .SetXYZM(ch.B_J_px[ibs], ch.B_J_py[ibs], ch.B_J_pz[ibs], ch.B_J_mass[ibs])
         MUMUP4_cjp = MU1P4_cjp + MU2P4_cjp
 
-        kaonP_P4_0c  .SetXYZM(ch.kaonP_px_0c[ibs], ch.kaonP_py_0c[ibs], ch.kaonP_pz_0c[ibs], PDG_KAON_MASS)
-        kaonP_P4_cjp .SetXYZM(ch.kaonP_px_cjp[ibs], ch.kaonP_py_cjp[ibs], ch.kaonP_pz_cjp[ibs], PDG_KAON_MASS)
-        kaonM_P4_0c  .SetXYZM(ch.kaonM_px_0c[ibs], ch.kaonM_py_0c[ibs], ch.kaonM_pz_0c[ibs], PDG_KAON_MASS)
-        kaonM_P4_cjp .SetXYZM(ch.kaonM_px_cjp[ibs], ch.kaonM_py_cjp[ibs], ch.kaonM_pz_cjp[ibs], PDG_KAON_MASS)
+##        kaonP_P4_0c  .SetXYZM(ch.kaonP_px_0c[ibs], ch.kaonP_py_0c[ibs], ch.kaonP_pz_0c[ibs], PDG_KAON_MASS)
+##        kaonP_P4_cjp .SetXYZM(ch.kaonP_px_cjp[ibs], ch.kaonP_py_cjp[ibs], ch.kaonP_pz_cjp[ibs], PDG_KAON_MASS)
+##        kaonM_P4_0c  .SetXYZM(ch.kaonM_px_0c[ibs], ch.kaonM_py_0c[ibs], ch.kaonM_pz_0c[ibs], PDG_KAON_MASS)
+##        kaonM_P4_cjp .SetXYZM(ch.kaonM_px_cjp[ibs], ch.kaonM_py_cjp[ibs], ch.kaonM_pz_cjp[ibs], PDG_KAON_MASS)
 
-        pionP4_0c  .SetXYZM(ch.pion_px_0c[ibs], ch.pion_py_0c[ibs], ch.pion_pz_0c[ibs], PDG_PION_MASS)
+##        pionP4_0c  .SetXYZM(ch.pion_px_0c[ibs], ch.pion_py_0c[ibs], ch.pion_pz_0c[ibs], PDG_PION_MASS)
 
-        BsP4_Cjp    .SetXYZM  ( ch.Bs_px_cjp[ibs], ch.Bs_py_cjp[ibs], ch.Bs_pz_cjp[ibs], ch.Bs_mass_cjp[ibs])
-        BcP4        .SetXYZM  ( ch.Bc_px[ibs], ch.Bc_py[ibs], ch.Bc_pz[ibs], ch.Bc_mass[ibs])
+##        BsP4_Cjp    .SetXYZM  ( ch.Bs_px_cjp[ibs], ch.Bs_py_cjp[ibs], ch.Bs_pz_cjp[ibs], ch.Bs_mass_cjp[ibs])
+##        BcP4        .SetXYZM  ( ch.Bc_px[ibs], ch.Bc_py[ibs], ch.Bc_pz[ibs], ch.Bc_mass[ibs])
 
         PV          = TVector3( ch.PV_bestBang_RF_X[ibs],   ch.PV_bestBang_RF_Y[ibs],   ch.PV_bestBang_RF_Z[ibs]    )
         PVE         = TVector3( sqrt(ch.PV_bestBang_RF_XE[ibs]),  sqrt(ch.PV_bestBang_RF_YE[ibs]),  sqrt(ch.PV_bestBang_RF_ZE[ibs])  )
@@ -148,15 +148,15 @@ for evt in range(0, nEvt):
         JPVE    = TVector3( sqrt(ch.B_J_DecayVtxXE[ibs]), sqrt(ch.B_J_DecayVtxYE[ibs]), sqrt(ch.B_J_DecayVtxZE[ibs])  )
         JPP3    = TVector3( ch.B_J_px[ibs],         ch.B_J_py[ibs],         ch.B_J_pz[ibs])
 
-        BsV_Cjp     = TVector3(ch.Bs_DecayVtxX[ibs],  ch.Bs_DecayVtxY[ibs],  ch.Bs_DecayVtxZ[ibs]   )
-        BsVE_Cjp    = TVector3( sqrt(ch.Bs_DecayVtxXE[ibs]), sqrt(ch.Bs_DecayVtxYE[ibs]), sqrt(ch.Bs_DecayVtxZE[ibs])  )
-        BsP3_Cjp    = BsP4_Cjp.Vect()
-
-        BcV    = TVector3(ch.Bc_DecayVtxX[ibs],  ch.Bc_DecayVtxY[ibs],  ch.Bc_DecayVtxZ[ibs]   )
-        BcVE   = TVector3( sqrt(ch.Bc_DecayVtxXE[ibs]), sqrt(ch.Bc_DecayVtxYE[ibs]), sqrt(ch.Bc_DecayVtxZE[ibs])  )
-        BcV_vtxfit     = TVector3(ch.Bc_DecayVtx_vtxfit_X[ibs],  ch.Bc_DecayVtx_vtxfit_Y[ibs],  ch.Bc_DecayVtx_vtxfit_Z[ibs]   )
-        BcVE_vtxfit    = TVector3( sqrt(ch.Bc_DecayVtx_vtxfit_XE[ibs]), sqrt(ch.Bc_DecayVtx_vtxfit_YE[ibs]), sqrt(ch.Bc_DecayVtx_vtxfit_ZE[ibs])  )
-        BcP3   = BcP4.Vect()
+##        BsV_Cjp     = TVector3(ch.Bs_DecayVtxX[ibs],  ch.Bs_DecayVtxY[ibs],  ch.Bs_DecayVtxZ[ibs]   )
+##        BsVE_Cjp    = TVector3( sqrt(ch.Bs_DecayVtxXE[ibs]), sqrt(ch.Bs_DecayVtxYE[ibs]), sqrt(ch.Bs_DecayVtxZE[ibs])  )
+##        BsP3_Cjp    = BsP4_Cjp.Vect()
+##
+##        BcV    = TVector3(ch.Bc_DecayVtxX[ibs],  ch.Bc_DecayVtxY[ibs],  ch.Bc_DecayVtxZ[ibs]   )
+##        BcVE   = TVector3( sqrt(ch.Bc_DecayVtxXE[ibs]), sqrt(ch.Bc_DecayVtxYE[ibs]), sqrt(ch.Bc_DecayVtxZE[ibs])  )
+##        BcV_vtxfit     = TVector3(ch.Bc_DecayVtx_vtxfit_X[ibs],  ch.Bc_DecayVtx_vtxfit_Y[ibs],  ch.Bc_DecayVtx_vtxfit_Z[ibs]   )
+##        BcVE_vtxfit    = TVector3( sqrt(ch.Bc_DecayVtx_vtxfit_XE[ibs]), sqrt(ch.Bc_DecayVtx_vtxfit_YE[ibs]), sqrt(ch.Bc_DecayVtx_vtxfit_ZE[ibs])  )
+##        BcP3   = BcP4.Vect()
 
 
 
@@ -275,156 +275,179 @@ for evt in range(0, nEvt):
     ###~~~~~~~~~~Kaons~~~~~~~~~~###
     #####~~~~~~~~~~~~~~~~~~~~~#####
 
-    	kaonP_pt_cjp[0] = kaonP_P4_cjp.Pt()
-    	kaonM_pt_cjp[0] = kaonM_P4_cjp.Pt()
-    	kaonP_pt_0c[0] = kaonP_P4_0c.Pt()
-    	kaonM_pt_0c[0] = kaonM_P4_0c.Pt()
-
-    	if kaonP_pt_0c[0] < 0.7  or kaonM_pt_0c[0] < 0.7 :continue
-        if abs(kaonP_P4_0c.Eta()) > 2.5 or abs(kaonM_P4_0c.Eta()) > 2.5 :continue
-
-
-        kaonP_track_normchi2[0] = ch.kaonP_track_normchi2[ibs]
-        kaonP_Hits[0] = ch.kaonP_Hits[ibs]
-        kaonP_PHits[0] = ch.kaonP_PHits[ibs]
-        kaonP_NTrackerLayers[0] = ch.kaonP_NTrackerLayers[ibs]
-        kaonP_NPixelLayers[0] = ch.kaonP_NPixelLayers[ibs]
-        kaonP_dxy_Bsdecay[0] = ch.kaonP_dxy_Bsdecay[ibs]
-        kaonP_dz_Bsdecay[0] = ch.kaonP_dz_Bsdecay[ibs]
-
-        kaonM_track_normchi2[0] = ch.kaonM_track_normchi2[ibs]
-        kaonM_Hits[0] = ch.kaonM_Hits[ibs]
-        kaonM_PHits[0] = ch.kaonM_PHits[ibs]
-        kaonM_NTrackerLayers[0] = ch.kaonM_NTrackerLayers[ibs]
-        kaonM_NPixelLayers[0] = ch.kaonM_NPixelLayers[ibs]
-        kaonM_dxy_Bsdecay[0] = ch.kaonM_dxy_Bsdecay[ibs]
-        kaonM_dz_Bsdecay[0] = ch.kaonM_dz_Bsdecay[ibs]
-
-    	deltaR_KpKm[0] = kaonP_P4_0c.DeltaR(kaonM_P4_0c)
+##    	kaonP_pt_cjp[0] = kaonP_P4_cjp.Pt()
+##    	kaonM_pt_cjp[0] = kaonM_P4_cjp.Pt()
+##    	kaonP_pt_0c[0] = kaonP_P4_0c.Pt()
+##    	kaonM_pt_0c[0] = kaonM_P4_0c.Pt()
+##
+##    	if kaonP_pt_0c[0] < 0.7  or kaonM_pt_0c[0] < 0.7 :continue
+##        if abs(kaonP_P4_0c.Eta()) > 2.5 or abs(kaonM_P4_0c.Eta()) > 2.5 :continue
+##
+##
+##        kaonP_track_normchi2[0] = ch.kaonP_track_normchi2[ibs]
+##        kaonP_Hits[0] = ch.kaonP_Hits[ibs]
+##        kaonP_PHits[0] = ch.kaonP_PHits[ibs]
+##        kaonP_NTrackerLayers[0] = ch.kaonP_NTrackerLayers[ibs]
+##        kaonP_NPixelLayers[0] = ch.kaonP_NPixelLayers[ibs]
+##        kaonP_dxy_Bsdecay[0] = ch.kaonP_dxy_Bsdecay[ibs]
+##        kaonP_dz_Bsdecay[0] = ch.kaonP_dz_Bsdecay[ibs]
+##
+##        kaonM_track_normchi2[0] = ch.kaonM_track_normchi2[ibs]
+##        kaonM_Hits[0] = ch.kaonM_Hits[ibs]
+##        kaonM_PHits[0] = ch.kaonM_PHits[ibs]
+##        kaonM_NTrackerLayers[0] = ch.kaonM_NTrackerLayers[ibs]
+##        kaonM_NPixelLayers[0] = ch.kaonM_NPixelLayers[ibs]
+##        kaonM_dxy_Bsdecay[0] = ch.kaonM_dxy_Bsdecay[ibs]
+##        kaonM_dz_Bsdecay[0] = ch.kaonM_dz_Bsdecay[ibs]
+##
+##    	deltaR_KpKm[0] = kaonP_P4_0c.DeltaR(kaonM_P4_0c)
 
     #####~~~~~~~~~~~~~~~~~~~~~#####
     ###~~~~~~~~~~Phi~~~~~~~~~~###
     #####~~~~~~~~~~~~~~~~~~~~~#####
 
-    	Phi_VtxProb[0] = ch.B_Phi_Prob[ibs]
-
-    	phi_mass_0c[0] = (kaonP_P4_0c + kaonM_P4_0c).M()
-    	phi_mass_cjp[0] = (kaonP_P4_cjp + kaonM_P4_cjp).M()
-
-        PDG_PHI_MASS        =   1.019455
-    	if abs(PDG_PHI_MASS - phi_mass_0c[0] ) > 0.015   :continue  #were 1.01-1.03
-
-    	phi_pt_0c[0] = (kaonP_P4_0c + kaonM_P4_0c).Pt()
-    	phi_pt_cjp[0] = (kaonP_P4_cjp + kaonM_P4_cjp).Pt()
+##    	Phi_VtxProb[0] = ch.B_Phi_Prob[ibs]
+##
+##    	phi_mass_0c[0] = (kaonP_P4_0c + kaonM_P4_0c).M()
+##    	phi_mass_cjp[0] = (kaonP_P4_cjp + kaonM_P4_cjp).M()
+##
+##        PDG_PHI_MASS        =   1.019455
+##    	if abs(PDG_PHI_MASS - phi_mass_0c[0] ) > 0.015   :continue  #were 1.01-1.03
+##
+##    	phi_pt_0c[0] = (kaonP_P4_0c + kaonM_P4_0c).Pt()
+##    	phi_pt_cjp[0] = (kaonP_P4_cjp + kaonM_P4_cjp).Pt()
 
 #####~~~~~~~~~~~~~~~~~~~~~~~~~~~~#####
 ###~~~~~~~~~~Bs and misc.~~~~~~~~~~###
 #####~~~~~~~~~~~~~~~~~~~~~~~~~~~~#####
 
-        Bs_mass_Cjp[0]          = ch.Bs_mass_cjp[ibs]
-        PDG_BS_MASS         =   5.36679
-        if abs(PDG_BS_MASS - Bs_mass_Cjp[0])  > 0.05   :continue # were 5.32 - 5.41
-
-        Bs_pt_Cjp[0]            = BsP4_Cjp.Pt()
-##        if Bs_pt_Cjp[0] <   10.0    :continue
-##        Bs_p_Cjp[0]             = BsP4_Cjp.Vect().Mag()
-
-        Bs_bcvtxDS2d_Cjp[0] = DetachSignificance2( BsV_Cjp - BcV, BcVE, BsVE_Cjp)
-        Bs_bcvtxDS2d_vtxfit[0] = DetachSignificance2( BsV_Cjp - BcV_vtxfit, BcVE_vtxfit, BsVE_Cjp)
-
-        Bs_pvDS2d_Cjp[0] = DetachSignificance2( BsV_Cjp - PV, PVE, BsVE_Cjp)
-        Bs_pv_detach_2D[0] =  sqrt( (BsV_Cjp - PV).X()**2 + (BsV_Cjp - PV).Y()**2)
-##        if Bs_bcvtxDS2d_Cjp[0] < 3. :continue
-
-        Bs_bcvtx_cos2_Cjp[0]        = DirectionCos2 ( BsV_Cjp - BcV, BsP3_Cjp )
-        Bs_bcvtx_cos2_vtxfit[0]        = DirectionCos2 ( BsV_Cjp - BcV_vtxfit, BsP3_Cjp )
-        if Bs_bcvtx_cos2_Cjp[0] < 0.9 :
-            H_cuts.Fill(9)
-            continue
-
-        Bs_pv_cos2_Cjp[0] = DirectionCos2(BsV_Cjp - PV, BsP3_Cjp)
-
-        Bs_vtxprob_Cjp[0]       = ch.Bs_Prob[ibs]
-        if Bs_vtxprob_Cjp[0] < 0.01 :
-            H_cuts.Fill(10)
-            continue
-
-        if abs(BsP4_Cjp.Eta())  > 2.5   :continue
-
-        Bs_Phi_cjp[0]            = BsP4_Cjp.Phi()
-        Bs_Eta_cjp[0]            = BsP4_Cjp.Eta()
-
-        BsVtx_Chi2[0] = ch.Bs_Chi2[ibs]; Bs_Bcdecay_weight[0] = ch.Bs_Bcdecay_weight[ibs];
+##        Bs_mass_Cjp[0]          = ch.Bs_mass_cjp[ibs]
+##        PDG_BS_MASS         =   5.36679
+##        if abs(PDG_BS_MASS - Bs_mass_Cjp[0])  > 0.05   :continue # were 5.32 - 5.41
+##
+##        Bs_pt_Cjp[0]            = BsP4_Cjp.Pt()
+####        if Bs_pt_Cjp[0] <   10.0    :continue
+####        Bs_p_Cjp[0]             = BsP4_Cjp.Vect().Mag()
+##
+##        Bs_bcvtxDS2d_Cjp[0] = DetachSignificance2( BsV_Cjp - BcV, BcVE, BsVE_Cjp)
+##        Bs_bcvtxDS2d_vtxfit[0] = DetachSignificance2( BsV_Cjp - BcV_vtxfit, BcVE_vtxfit, BsVE_Cjp)
+##
+##        Bs_pvDS2d_Cjp[0] = DetachSignificance2( BsV_Cjp - PV, PVE, BsVE_Cjp)
+##        Bs_pv_detach_2D[0] =  sqrt( (BsV_Cjp - PV).X()**2 + (BsV_Cjp - PV).Y()**2)
+####        if Bs_bcvtxDS2d_Cjp[0] < 3. :continue
+##
+##        Bs_bcvtx_cos2_Cjp[0]        = DirectionCos2 ( BsV_Cjp - BcV, BsP3_Cjp )
+##        Bs_bcvtx_cos2_vtxfit[0]        = DirectionCos2 ( BsV_Cjp - BcV_vtxfit, BsP3_Cjp )
+##        if Bs_bcvtx_cos2_Cjp[0] < 0.9 :
+##            H_cuts.Fill(9)
+##            continue
+##
+##        Bs_pv_cos2_Cjp[0] = DirectionCos2(BsV_Cjp - PV, BsP3_Cjp)
+##
+##        Bs_vtxprob_Cjp[0]       = ch.Bs_Prob[ibs]
+##        if Bs_vtxprob_Cjp[0] < 0.01 :
+##            H_cuts.Fill(10)
+##            continue
+##
+##        if abs(BsP4_Cjp.Eta())  > 2.5   :continue
+##
+##        Bs_Phi_cjp[0]            = BsP4_Cjp.Phi()
+##        Bs_Eta_cjp[0]            = BsP4_Cjp.Eta()
+##
+##        BsVtx_Chi2[0] = ch.Bs_Chi2[ibs]; Bs_Bcdecay_weight[0] = ch.Bs_Bcdecay_weight[ibs];
 
 #####~~~~~~~~~~~~~~~~~~~~#####
 ###~~~~~~~~~~Pion~~~~~~~~~~###
 #####~~~~~~~~~~~~~~~~~~~~#####
 
-        pion_pt_0c[0] = pionP4_0c.Pt()
-        pion_track_normchi2[0] = ch.pion_track_normchi2[ibs]
-        pion_Hits[0] = ch.pion_Hits[ibs]
-        pion_PHits[0] = ch.pion_PHits[ibs]
-        pion_NTrackerLayers[0] = ch.pion_NTrackerLayers[ibs]
-        pion_NPixelLayers[0] = ch.pion_NPixelLayers[ibs]
-        pion_dxy_Bcdecay[0] = ch.pion_dxy_Bcdecay[ibs]
-        pion_dz_Bcdecay[0] = ch.pion_dz_Bcdecay[ibs]
-        pion_Bcdecay_weight[0] = ch.pion_Bcdecay_weight[ibs]
-	deltaR_piBs[0] = pionP4_0c.DeltaR(BsP4_Cjp)
+##        pion_pt_0c[0] = pionP4_0c.Pt()
+##        pion_track_normchi2[0] = ch.pion_track_normchi2[ibs]
+##        pion_Hits[0] = ch.pion_Hits[ibs]
+##        pion_PHits[0] = ch.pion_PHits[ibs]
+##        pion_NTrackerLayers[0] = ch.pion_NTrackerLayers[ibs]
+##        pion_NPixelLayers[0] = ch.pion_NPixelLayers[ibs]
+##        pion_dxy_Bcdecay[0] = ch.pion_dxy_Bcdecay[ibs]
+##        pion_dz_Bcdecay[0] = ch.pion_dz_Bcdecay[ibs]
+##        pion_Bcdecay_weight[0] = ch.pion_Bcdecay_weight[ibs]
+##	deltaR_piBs[0] = pionP4_0c.DeltaR(BsP4_Cjp)
 
 #####~~~~~~~~~~~~~~~~~~~~~~~~~~~~#####
 ###~~~~~~~~~~Bc and misc.~~~~~~~~~~###
 #####~~~~~~~~~~~~~~~~~~~~~~~~~~~~#####
 
-        Bc_mass[0]          = ch.Bc_mass[ibs]
-        Bc_mass_delta[0]    = ch.Bc_mass[ibs] - ch.Bs_mass_cjp[ibs] + PDG_BS_MASS
-        PDG_BC_MASS         =   6.2751
-        if abs(PDG_BC_MASS - Bc_mass[0])   >   0.5    :continue   #were 5.975-6.575
+##        Bc_mass[0]          = ch.Bc_mass[ibs]
+##        Bc_mass_delta[0]    = ch.Bc_mass[ibs] - ch.Bs_mass_cjp[ibs] + PDG_BS_MASS
+##        PDG_BC_MASS         =   6.2751
+##        if abs(PDG_BC_MASS - Bc_mass[0])   >   0.5    :continue   #were 5.975-6.575
+##
+##        Bc_pt[0]            = BcP4.Pt()
+##
+##        Bc_pvDS2d[0] = DetachSignificance2( BcV - PV, PVE, BcVE)
+##        Bc_pvDS2d_vtxfit[0] = DetachSignificance2( BcV_vtxfit - PV, PVE, BcVE_vtxfit)
+##        Bc_pv_detach_2D[0] =  sqrt( (BcV - PV).X()**2 + (BcV - PV).Y()**2)
+##        Bc_pv_detach_2D_vtxfit[0] =  sqrt( (BcV_vtxfit - PV).X()**2 + (BcV_vtxfit - PV).Y()**2)
+####        if Bc_pvDS2d[0] < 3. :continue
+##
+##        Bc_pvcos2[0]        = DirectionCos2 ( BcV - PV, BcP3 )
+##        Bc_pvcos2_vtxfit[0]        = DirectionCos2 ( BcV_vtxfit - PV, BcP3 )
+##        if Bc_pvcos2[0] < 0.9 :
+##            H_cuts.Fill(9)
+##            continue
+##
+##        Bc_vtxprob[0]       = ch.Bc_Prob[ibs]
+##        if Bc_vtxprob[0] < 0.01 :
+##            H_cuts.Fill(10)
+##            continue
+##
+##        if abs(BcP4.Eta())  > 2.5   :continue
+##
+##        Bc_Phi[0]            = BcP4.Phi()
+##        Bc_Eta[0]            = BcP4.Eta()
+##
+##        BcVertex_isValid[0] = ch.BcVertex_isValid[ibs]
+##        BcVtx_Chi2_kinfit[0] = ch.Bc_Chi2[ibs];   BcVtx_Chi2_vtxfit[0] = ch.BcVertex_Chi[ibs]; BcVtx_normChi2_vtxfit[0] = ch.BcVertex_normChi[ibs];
+##
+##
+##        Bc_DecayVtxX[0] = ch.Bc_DecayVtxX[ibs]
+##        Bc_DecayVtxY[0] = ch.Bc_DecayVtxY[ibs]
+##        Bc_DecayVtxZ[0] = ch.Bc_DecayVtxZ[ibs]
+##        Bc_DecayVtxXE[0] = ch.Bc_DecayVtxXE[ibs]
+##        Bc_DecayVtxYE[0] = ch.Bc_DecayVtxYE[ibs]
+##        Bc_DecayVtxZE[0] = ch.Bc_DecayVtxZE[ibs]
+##
+##        Bc_DecayVtx_vtxfit_X[0] = ch.Bc_DecayVtx_vtxfit_X[ibs]
+##        Bc_DecayVtx_vtxfit_Y[0] = ch.Bc_DecayVtx_vtxfit_Y[ibs]
+##        Bc_DecayVtx_vtxfit_Z[0] = ch.Bc_DecayVtx_vtxfit_Z[ibs]
+##        Bc_DecayVtx_vtxfit_XE[0] = ch.Bc_DecayVtx_vtxfit_XE[ibs]
+##        Bc_DecayVtx_vtxfit_YE[0] = ch.Bc_DecayVtx_vtxfit_YE[ibs]
+##        Bc_DecayVtx_vtxfit_ZE[0] = ch.Bc_DecayVtx_vtxfit_ZE[ibs]
+##
+##
+##        PV_refit_prob[0] = ch.PV_bestBang_RF_CL[ibs]
+##        PV_bestBang_RF_X[0] = ch.PV_bestBang_RF_X[ibs]
 
-        Bc_pt[0]            = BcP4.Pt()
 
-        Bc_pvDS2d[0] = DetachSignificance2( BcV - PV, PVE, BcVE)
-        Bc_pvDS2d_vtxfit[0] = DetachSignificance2( BcV_vtxfit - PV, PVE, BcVE_vtxfit)
-        Bc_pv_detach_2D[0] =  sqrt( (BcV - PV).X()**2 + (BcV - PV).Y()**2)
-        Bc_pv_detach_2D_vtxfit[0] =  sqrt( (BcV_vtxfit - PV).X()**2 + (BcV_vtxfit - PV).Y()**2)
-##        if Bc_pvDS2d[0] < 3. :continue
+#####~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#####
+###~~~~~~~~~~Photons and misc.~~~~~~~~~~###
+#####~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#####
 
-        Bc_pvcos2[0]        = DirectionCos2 ( BcV - PV, BcP3 )
-        Bc_pvcos2_vtxfit[0]        = DirectionCos2 ( BcV_vtxfit - PV, BcP3 )
-        if Bc_pvcos2[0] < 0.9 :
-            H_cuts.Fill(9)
-            continue
+        B0_mass[0]         = ch.B0_mass[ibs]
+        pi0_mass[0]        = ch.pi0_mass[ibs]
 
-        Bc_vtxprob[0]       = ch.Bc_Prob[ibs]
-        if Bc_vtxprob[0] < 0.01 :
-            H_cuts.Fill(10)
-            continue
+        pho1_Eta[0]        = ch.pho1_Eta[ibs]
+        pho1_Phi[0]        = ch.pho1_Phi[ibs]
+        pho1_Pt[0]         = ch.pho1_Pt[ibs]
+        pho1_E[0]          = ch.pho1_E[ibs]
+        pho1_Et[0]         = ch.pho1_Et[ibs]
+        pho1_Jpsi_cos[0]   = ch.pho1_Jpsi_cos[ibs]
 
-        if abs(BcP4.Eta())  > 2.5   :continue
-
-        Bc_Phi[0]            = BcP4.Phi()
-        Bc_Eta[0]            = BcP4.Eta()
-
-        BcVertex_isValid[0] = ch.BcVertex_isValid[ibs]
-        BcVtx_Chi2_kinfit[0] = ch.Bc_Chi2[ibs];   BcVtx_Chi2_vtxfit[0] = ch.BcVertex_Chi[ibs]; BcVtx_normChi2_vtxfit[0] = ch.BcVertex_normChi[ibs];
-
-
-        Bc_DecayVtxX[0] = ch.Bc_DecayVtxX[ibs]
-        Bc_DecayVtxY[0] = ch.Bc_DecayVtxY[ibs]
-        Bc_DecayVtxZ[0] = ch.Bc_DecayVtxZ[ibs]
-        Bc_DecayVtxXE[0] = ch.Bc_DecayVtxXE[ibs]
-        Bc_DecayVtxYE[0] = ch.Bc_DecayVtxYE[ibs]
-        Bc_DecayVtxZE[0] = ch.Bc_DecayVtxZE[ibs]
-
-        Bc_DecayVtx_vtxfit_X[0] = ch.Bc_DecayVtx_vtxfit_X[ibs]
-        Bc_DecayVtx_vtxfit_Y[0] = ch.Bc_DecayVtx_vtxfit_Y[ibs]
-        Bc_DecayVtx_vtxfit_Z[0] = ch.Bc_DecayVtx_vtxfit_Z[ibs]
-        Bc_DecayVtx_vtxfit_XE[0] = ch.Bc_DecayVtx_vtxfit_XE[ibs]
-        Bc_DecayVtx_vtxfit_YE[0] = ch.Bc_DecayVtx_vtxfit_YE[ibs]
-        Bc_DecayVtx_vtxfit_ZE[0] = ch.Bc_DecayVtx_vtxfit_ZE[ibs]
-
-
-        PV_refit_prob[0] = ch.PV_bestBang_RF_CL[ibs]
-        PV_bestBang_RF_X[0] = ch.PV_bestBang_RF_X[ibs]
+        pho2_Eta[0]        = ch.pho2_Eta[ibs]
+        pho2_Phi[0]        = ch.pho2_Phi[ibs]
+        pho2_Pt[0]         = ch.pho2_Pt[ibs]
+        pho2_E[0]          = ch.pho2_E[ibs]
+        pho2_Et[0]         = ch.pho2_Et[ibs]
+        pho2_Jpsi_cos[0]   = ch.pho2_Jpsi_cos[ibs]
+           
 #---------------------------------------------------
 
         _mctr = 0
